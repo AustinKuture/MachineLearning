0c42ab7f58a162f5feb55af6c3b972dae5af034c
Kuture <austinkuture@126.com>
2018-12-20 18:19:37 +0800

    RNN语音识别可以正常使用

--- RNN/test.py
+++ RNN/test.py
@@ -10,18 +10,18 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 conf = Config()
 
-wav_files, text_labels = utils.get_wavs_lables()
+# wav_files, text_labels = utils.get_wavs_lables()
 
-words_size, words, word_num_map = utils.create_dict(text_labels)
+# words_size, words, word_num_map = utils.create_dict(text_labels)
 
-bi_rnn = BiRNN(wav_files, text_labels, words_size, words, word_num_map)
-bi_rnn.build_test()
+# bi_rnn = BiRNN(wav_files, text_labels, words_size, words, word_num_map)
+# bi_rnn.build_test()
 
-# wav_files = ['E:\\wav\\train\\A2\\A2_11.wav']
-# txt_labels = ['北京 丰台区 农民 自己 花钱 筹办 万 佛 延寿 寺 迎春 庙会 吸引 了 区内 六十 支 秧歌队 参赛']
-# words_size, words, word_num_map = utils.create_dict(txt_labels)
-# bi_rnn = BiRNN(wav_files, txt_labels, words_size, words, word_num_map)
-# bi_rnn.build_target_wav_file_test(wav_files, txt_labels)
+wav_files = ['/Users/microduino/Desktop/RNN/wav/test/1.wav']
+txt_labels = ['贝多芬 交响曲 音乐 古典']
+words_size, words, word_num_map = utils.create_dict(txt_labels)
+bi_rnn = BiRNN(wav_files, txt_labels, words_size, words, word_num_map)
+bi_rnn.build_target_wav_file_test(wav_files, txt_labels)
 
 
 
